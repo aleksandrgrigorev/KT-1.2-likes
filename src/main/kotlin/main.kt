f54ecae7ad7b0a63747f035b8fd2978ef4b15704
aleksandrgrigorev <xundr1992@gmail.com>
2022-06-30 18:12:36 +0400

    Commit #2: not that easy!

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main() {
     println("Введите число:")
     val likes = readln()
-    val people = if (likes.endsWith("1")) " человеку" else " людям"
+    val people = if (likes.endsWith("1") && !likes.endsWith("11")) " человеку" else " людям"
     print("Понравилось $likes$people")
 }
